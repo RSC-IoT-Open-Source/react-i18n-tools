2a0de80360ccb69e32b4b6770db58e016c7ed448
kbruck-rsc <kbruck@petsafe.net>
2019-02-25 11:24:25 -0500

    working on styling injections ([styling])

--- components/Trext.js
+++ components/Trext.js
@@ -4,24 +4,27 @@ const TrextContext = require('./TrextContext')
 let TextComponent = undefined
 
 module.exports = ({ Text, style, id, children }) => {
-    if (TextComponent === undefined && Text === undefined) {
-        throw new Error('You must initialize Trext with a Component to wrap text in. In React Native this is a Text component.')
-    } else if (TextComponent === undefined && Text !== undefined) {
-        TextComponent = Text
-    } else {
+  if (TextComponent === undefined && Text === undefined) {
+    throw new Error('You must initialize Trext with a Component to wrap text in. In React Native this is a Text component.')
+  } else if (TextComponent === undefined && Text !== undefined) {
+    TextComponent = Text
+  } else {
+    return React.createElement(
+      TrextContext.Consumer,
+      {},
+      ({ translations, locale }) => {
+        if (translations[locale] === undefined || translations[locale].translated === undefined || translations[locale].translated[id] === undefined) {
+          return React.createElement(TextComponent, { style: style }, applyNewLines(children))
+        }
         return React.createElement(
-            TrextContext.Consumer,
-            {},
-            ({ translations, locale }) => {
-                if (translations[locale] === undefined || translations[locale].translated === undefined || translations[locale].translated[id] === undefined) {
-                    return React.createElement(TextComponent, { style: style }, children)
-                }
-                return React.createElement(
-                    TextComponent,
-                    { style: style },
-                    translations[locale].translated[id].translated
-                )
-            }
+          TextComponent,
+          { style: style },
+          applyNewLines(translations[locale].translated[id].translated)
         )
-    }
+      }
+    )
+  }
+}
+const applyNewLines = (text) => {
+  return text.split('[\n]').replace('\n')
 }
